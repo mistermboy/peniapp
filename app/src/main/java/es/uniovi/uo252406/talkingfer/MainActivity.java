2b8705f2d41861b4ab3e014fa1f5f272816be20b
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-07-14 00:14:38 +0200

    random value

--- app/src/main/java/es/uniovi/uo252406/talkingfer/MainActivity.java
+++ app/src/main/java/es/uniovi/uo252406/talkingfer/MainActivity.java
@@ -4,6 +4,7 @@ package es.uniovi.uo252406.talkingfer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,17 +20,44 @@ public class MainActivity extends AppCompatActivity {
 
         btnPrincipal = (Button) findViewById(R.id.btnPrincipal);
 
-
-        mp = MediaPlayer.create(this,R.raw.fer1);
-
         btnPrincipal.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                mp.start();
+                changeAudio(); mp.start();
             }
         });
 
 
     }
+
+    private void changeAudio(){
+
+        int valor = (int) (Math.random() * 17) + 1;
+
+        String val = ""+valor;
+
+
+        Log.i("Valor",val);
+
+        if(valor == 1)
+            mp = MediaPlayer.create(this,R.raw.fer1);
+        if(valor == 2)
+            mp = MediaPlayer.create(this,R.raw.fer2);
+        if(valor == 3)
+            mp = MediaPlayer.create(this,R.raw.fer3);
+        if(valor == 4)
+            mp = MediaPlayer.create(this,R.raw.fer4);
+        if(valor == 5)
+            mp = MediaPlayer.create(this,R.raw.fer5);
+        if(valor == 6)
+            mp = MediaPlayer.create(this,R.raw.fer6);
+        if(valor == 7)
+            mp = MediaPlayer.create(this,R.raw.fer7);
+        if(valor == 8)
+            mp = MediaPlayer.create(this,R.raw.fer8);
+        if(valor == 9)
+            mp = MediaPlayer.create(this,R.raw.fer9);
+    }
+
 }
