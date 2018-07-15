de427ba3316beaec4b0da2e566622d12b1f60fe8
Guillermo Lopez Rodriguez (GLR) <guillermin97@gmail.com>
2018-07-15 22:16:37 +0200

    1. Arreglada superposicion de audio. 2. Agregados todos los audios disponibles. 3. Al finalizar de reproducir todos vuelve a empezar de nuevo la lista. 4. Eliminado cast innecesario.

--- app/src/main/java/es/uniovi/uo252406/talkingfer/MainActivity.java
+++ app/src/main/java/es/uniovi/uo252406/talkingfer/MainActivity.java
@@ -1,6 +1,5 @@
 package es.uniovi.uo252406.talkingfer;
 
-
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,19 +12,27 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mp;
     Button btnPrincipal;
     int valor = 1;
+    final int maxValor = 17;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnPrincipal = (Button) findViewById(R.id.btnPrincipal);
+        btnPrincipal = findViewById(R.id.btnPrincipal);
 
         btnPrincipal.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                changeAudio(); mp.start();
+                if(mp != null)
+                    mpNull();
+                changeAudio();
+                try{
+                    mp.start();
+                }catch(IllegalStateException e){
+                    Log.e("IllegalStateException", "Illegal State Exception: " + e.getMessage());
+                }
             }
         });
 
@@ -33,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changeAudio(){
-
-
+        if(valor > maxValor){
+            valor = 1;
+            mpNull();
+        }
         if(valor == 1)
             mp = MediaPlayer.create(this,R.raw.fer1);
         if(valor == 2)
@@ -53,8 +62,37 @@ public class MainActivity extends AppCompatActivity {
             mp = MediaPlayer.create(this,R.raw.fer8);
         if(valor == 9)
             mp = MediaPlayer.create(this,R.raw.fer9);
-
+        if(valor == 10)
+            mp = MediaPlayer.create(this,R.raw.fer10);
+        if(valor == 11)
+            mp = MediaPlayer.create(this,R.raw.fer11);
+        if(valor == 12)
+            mp = MediaPlayer.create(this,R.raw.fer12);
+        if(valor == 13)
+            mp = MediaPlayer.create(this,R.raw.fer13);
+        if(valor == 14)
+            mp = MediaPlayer.create(this,R.raw.fer14);
+        if(valor == 15)
+            mp = MediaPlayer.create(this,R.raw.fer15);
+        if(valor == 16)
+            mp = MediaPlayer.create(this,R.raw.fer16);
+        if(valor == 17)
+            mp = MediaPlayer.create(this,R.raw.fer17);
         valor++;
+
+    }
+
+    private void mpNull(){
+        try{
+            mp.stop();
+            mp.release();
+            mp = null;
+        }catch(IllegalStateException e){
+            Log.e("IllegalStateException", "Illegal State Exception: " + e.getMessage());
+        }catch(Exception e){
+            Log.e("Exception", "General Exception: " + e.getMessage());
+        }
+
     }
 
 }
