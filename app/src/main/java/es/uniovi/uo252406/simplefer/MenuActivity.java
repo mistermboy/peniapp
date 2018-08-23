e48e5af9cc6c42849ab8a0a36103132c9c258d4d
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-08-23 14:58:14 +0200

    nav_bar

--- app/src/main/java/es/uniovi/uo252406/simplefer/MenuActivity.java
+++ app/src/main/java/es/uniovi/uo252406/simplefer/MenuActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import es.uniovi.uo252406.simplefer.Entities.Player;
 import es.uniovi.uo252406.simplefer.Fragments.FreeSelectionFragment;
@@ -17,6 +20,8 @@ import es.uniovi.uo252406.simplefer.Fragments.PrincipalPersonFragment;
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    String person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,12 @@ public class MenuActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Recogemos los datos
+        Bundle b = getIntent().getExtras();
+        person = (String) b.getString("person");
+
+        asignaInfo(navigationView);
 
         FragmentManager fm = getSupportFragmentManager();
 
@@ -64,11 +75,51 @@ public class MenuActivity extends AppCompatActivity
         } else if (id == R.id.audios) {
             Player.getInstance().pause();
             fm.beginTransaction().replace(R.id.escenario,new FreeSelectionFragment()).commit();
+        }else if (id == R.id.inicio) {
+            Player.getInstance().pause();
+            finish();
         }
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    /**
+     * Asigna información del usuario elegido en el menú lateral
+     * @param navigationView
+     */
+    private void asignaInfo(NavigationView navigationView) {
+
+
+        View headView = navigationView.getHeaderView(0);
+
+        ImageView perfil = (ImageView) headView.findViewById(R.id.iPerfil);
+        TextView name = (TextView) headView.findViewById(R.id.textName);
+        TextView description = (TextView) headView.findViewById(R.id.textDescript);
+
+        int rawID = getResources().getIdentifier(person+"cara","drawable",getPackageName());
+        perfil.setImageResource(rawID);
+
+        if(person.equals("fer")) {
+            name.setText("Fernando Amado");
+            name.setTextColor(getResources().getColor(R.color.white));
+
+            description.setText("Español ante todo. La patría siempre por delante");
+            description.setTextColor(getResources().getColor(R.color.white));
+
+        }else if(person.equals("berto")){
+            name.setText("Alberto García");
+            name.setTextColor(getResources().getColor(R.color.white));
+
+            description.setText("Drogopropulsado. Subo videos de mi gato a insta");
+            description.setTextColor(getResources().getColor(R.color.white));
+        }
+
+
     }
 
 
