b240a04ae1757550f9c74ac303b5c77edddb94ca
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-08-25 22:31:10 +0200

    working on quiz

---
+++ app/src/main/java/es/uniovi/uo252406/simplefer/Fragments/QuizFragment.java
@@ -0,0 +1,33 @@
+package es.uniovi.uo252406.simplefer.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.uniovi.uo252406.simplefer.R;
+
+
+public class QuizFragment extends Fragment {
+
+    private View view;
+
+    public QuizFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        return view;
+    }
+
+
+}
