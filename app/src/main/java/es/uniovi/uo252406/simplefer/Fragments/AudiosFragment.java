0d42ad5ae7c3850162cf1b95fd3223a24068d6d0
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-08-30 22:35:43 +0200

    working on permissons

--- app/src/main/java/es/uniovi/uo252406/simplefer/Fragments/AudiosFragment.java
+++ app/src/main/java/es/uniovi/uo252406/simplefer/Fragments/AudiosFragment.java
@@ -1,11 +1,22 @@
 package es.uniovi.uo252406.simplefer.Fragments;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +30,7 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import es.uniovi.uo252406.simplefer.Logical.Player;
@@ -31,6 +43,8 @@ public class AudiosFragment extends android.support.v4.app.Fragment {
 
     ArrayList<String> audios;
     String person;
+
+    private final int REQUEST_ACCESS_FINE =0;
 
 
     public AudiosFragment() {
@@ -180,6 +194,94 @@ public class AudiosFragment extends android.support.v4.app.Fragment {
             b2.setTypeface(typeface);
             b3.setTypeface(typeface);
             b4.setTypeface(typeface);
+
+
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+            b2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                    int rawID = getContext().getResources().getIdentifier("fer_ay_que_tonto_eres", "raw", getContext().getPackageName());
+                    Uri newUri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + rawID);
+
+
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.WRITE_SETTINGS)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                      //  ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_ACCESS_FINE);
+                        ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.WRITE_SETTINGS},REQUEST_ACCESS_FINE);
+
+                        Log.i("entra","IFFFF");
+
+                    }else{
+
+                        Log.i("entra","ELSEEEEEE");
+
+                        File k = new File(Environment.getExternalStorageDirectory(), "fer_ay_que_tonto_eres.mp3"); // path is a file to /sdcard/media/ringtone
+
+                        ContentValues values = new ContentValues();
+                        values.put(MediaStore.MediaColumns.DATA, k.getAbsolutePath());
+                        values.put(MediaStore.MediaColumns.TITLE, "My Song title");
+                        values.put(MediaStore.MediaColumns.SIZE, 215454);
+                        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3");
+                        values.put(MediaStore.Audio.Media.ARTIST, "Madonna");
+                        values.put(MediaStore.Audio.Media.DURATION, 230);
+                        values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
+                        values.put(MediaStore.Audio.Media.IS_NOTIFICATION, false);
+                        values.put(MediaStore.Audio.Media.IS_ALARM, false);
+                        values.put(MediaStore.Audio.Media.IS_MUSIC, false);
+
+                        //Insert it into the database
+                        Uri uri = MediaStore.Audio.Media.getContentUriForPath(k.getAbsolutePath());
+                        newUri = getActivity().getContentResolver().insert(uri, values);
+
+                        RingtoneManager.setActualDefaultRingtoneUri(
+                                getActivity(),
+                                RingtoneManager.TYPE_RINGTONE,
+                                newUri
+                        );
+
+
+                        //fetch default Ringtone
+                        Ringtone defaultRingtone = RingtoneManager.getRingtone(getActivity(),
+                                Settings.System.DEFAULT_RINGTONE_URI);
+                        //fetch current Ringtone
+                        Uri currentRintoneUri = RingtoneManager.getActualDefaultRingtoneUri(getActivity()
+                                .getApplicationContext(), RingtoneManager.TYPE_RINGTONE);
+                        Ringtone currentRingtone = RingtoneManager.getRingtone(getActivity(), currentRintoneUri);
+
+                        //play current Ringtone
+                        currentRingtone.play();
+
+                    }
+                }
+            });
+
+            b3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+            b4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+
 
 
             return true;
